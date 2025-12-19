2f0986da515f57c3a93ffce12d2d9921245182d2
kensey <microsoft.ashim@gmail.com>
2025-12-19 22:02:50 +0500

    Added report, changed defaults in db structure

--- src/db_utils.py
+++ src/db_utils.py
@@ -13,34 +13,42 @@ def init_db(sqlite_path: str) -> None:
     cur = conn.cursor()
 
     cur.execute("""
-    CREATE TABLE IF NOT EXISTS events (
-      zone TEXT NOT NULL,
-      datetime TEXT NOT NULL,
-      signal TEXT NOT NULL,
-      value REAL,
-      unit TEXT,
-      is_estimated INTEGER DEFAULT 0,
-      estimation_method TEXT,
-      queried_at TEXT,
-      ingested_at TEXT,
-      raw_json TEXT,
-      UNIQUE(zone, datetime, signal)
-    );
+      CREATE TABLE IF NOT EXISTS events (
+        zone TEXT NOT NULL,
+        datetime TEXT NOT NULL,
+        signal TEXT NOT NULL,
+
+        value REAL NOT NULL,
+        unit TEXT NOT NULL,
+
+        is_estimated INTEGER NOT NULL DEFAULT 0,
+        estimation_method TEXT NOT NULL DEFAULT 'NOT_ESTIMATED',
+
+        queried_at TEXT NOT NULL,
+        ingested_at TEXT NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+
+        raw_json TEXT NOT NULL,
+
+        UNIQUE(zone, datetime, signal),
+      );
     """)
 
     cur.execute("""
-    CREATE TABLE IF NOT EXISTS daily_summary (
-      day TEXT NOT NULL,
-      zone TEXT NOT NULL,
-      signal TEXT NOT NULL,
-      count INTEGER NOT NULL,
-      min REAL,
-      max REAL,
-      avg REAL,
-      unit TEXT,
-      computed_at TEXT,
-      UNIQUE(day, zone, signal)
-    );
+      CREATE TABLE IF NOT EXISTS daily_summary (
+        day TEXT NOT NULL,
+        zone TEXT NOT NULL,
+        signal TEXT NOT NULL,
+
+        count INTEGER NOT NULL,
+        min REAL NOT NULL,
+        max REAL NOT NULL,
+        avg REAL NOT NULL,
+        unit TEXT NOT NULL,
+
+        computed_at TEXT NOT NULL DEFAULT (CURRENT_TIMESTAMP),
+
+        UNIQUE(day, zone, signal),
+      );
     """)
 
     conn.commit()
